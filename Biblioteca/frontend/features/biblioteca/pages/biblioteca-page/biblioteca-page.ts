2fff15d49138db7b9b691eddb4294e058d41e01e
selinajxc <smarcj2005@gmail.com>
2026-05-20 09:12:17 -0600

    Agregar estructura base inicial Angular

---
+++ Biblioteca/frontend/features/biblioteca/pages/biblioteca-page/biblioteca-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-biblioteca-page',
+  imports: [],
+  templateUrl: './biblioteca-page.html',
+  styleUrl: './biblioteca-page.scss',
+})
+export class BibliotecaPage {
+
+}
