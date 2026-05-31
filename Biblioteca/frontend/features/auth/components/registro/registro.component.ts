b1f341610c5304366375f45b3814d837cd252e0d
xcaretcristal <xcaretcristalmv@gmail.com>
2026-05-31 13:10:08 -0600

    Se le coloco diseño a mi vista de crear sesión

--- Biblioteca/frontend/features/auth/components/registro/registro.component.ts
+++ Biblioteca/frontend/features/auth/components/registro/registro.component.ts
@@ -5,33 +5,35 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
   imports: [
-    CommonModule, 
-    FormsModule, 
-    MatInputModule, 
-    MatButtonModule, 
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
     MatIconModule,
-    RouterModule // Necesario para usar routerLink en el HTML
+    MatFormFieldModule,
+    RouterModule
   ],
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss']
 })
 export class RegistroComponent {
+
   nombre: string = '';
   correo: string = '';
   password: string = '';
   confirmPassword: string = '';
-  
-  // Control individual para cada "ojito"
+
   ocultarPassword = true;
   ocultarConfirmPassword = true;
 
   registrar() {
-    // Doble validación por seguridad
+
     if (this.password !== this.confirmPassword) {
       console.error('Las contraseñas no coinciden');
       return;
@@ -42,6 +44,7 @@ export class RegistroComponent {
       correo: this.correo,
       password: this.password
     });
-    // Aquí conectaremos con el backend
+
+    // Aquí irá la conexión con el backend
   }
 }
